c56da62c09d1f8f18ed76fd705488803924af3ae
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2025-01-27 19:05:37 -0300

    commit

--- projectJs01/script.js
+++ projectJs01/script.js
@@ -1,6 +1,17 @@
+const randomNumber = Math.round(Math.random( )* 10);
+let xAttempts = 1; //numero de tentativas
+
 function handleClick(event){
     event.preventDefault(); //Não reccaregar a page quando apertar o botão
     const inputNumber = document.querySelector("#inputNumber");
+ 
 
-    console.log(inputNumber.value);
+    if(Number(inputNumber.value) == randomNumber){
+        document.querySelector(".screen1").classList.add("hide");
+        document.querySelector(".screen2").classList.remove("hide");
+        document.querySelector(".screen2 h2")
+        .innerText = ("Você acertou em " + xAttempts + "tentativas");
+    }
+
+    xAttempts++;
 }
