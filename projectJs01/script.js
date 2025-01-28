801ca7bdc79d990a8bfa490f2720f44b133eed50
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2025-01-28 17:29:32 -0300

    refactoring and finalization

--- projectJs01/script.js
+++ projectJs01/script.js
@@ -1,8 +1,20 @@
+//variaveis
 const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
+const btnTry = querySelector("#btnTry");
+const btnReset = querySelector("#btnReset");
 
 const randomNumber = Math.round(Math.random( )* 10);
 let xAttempts = 1; //numero de tentativas
+//eventos
+btnTry.addEventListener('click',handleTryClick);
+btnReset.addEventListener('click',handleResetclick);
+document.addEventListener('keydown', function(e){
+    if(e.key == 'Enter'){
+        handleResetclick();
+    }
+})
+
 //função callback
 function handleTryClick(event){
     event.preventDefault(); //Não fazer o padrao da tag html, no caso do botão no form, não enviar nada;
@@ -10,10 +22,8 @@ function handleTryClick(event){
  
 
     if(Number(inputNumber.value) == randomNumber){
-        screen1.classList.add("hide");
-        screen2.classList.remove("hide");
-
-        document.querySelector(".screen2 h2")
+        toggleScreen();
+        screen2.querySelector("h2")
         .innerText = ("Você acertou em " + xAttempts + "tentativas");
     }
 
@@ -21,14 +31,15 @@ function handleTryClick(event){
     inputNumber.value = "";
 }
 
-//eventos
-const btnTry = querySelector("#btnTry");
-const btnReset = querySelector("#btnReset");
-
-btnTry.addEventListener('click',handleTryClick);
-btnReset.addEventListener('click',function(){
-    screen1.classList.remove("hide");
-    screen2.classList.add("hide");
+function handleResetclick(){
+    toggleScreen();
     xAttempts = 1;
-});
+
+}
+function toggleScreen(){
+    screen1.classList.toggle("hide");
+    screen2.classList.toggle("hide");
+}
+
+
 
