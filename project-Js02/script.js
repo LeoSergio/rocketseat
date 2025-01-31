60a09e25895ce7d36cff0d9e2ae625cd1b051477
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2025-01-31 10:42:23 -0300

    add Js

--- project-Js02/script.js
+++ project-Js02/script.js
@@ -1,1 +1,15 @@
-const peso = document.querySelector.value('#kg');
+//variaveis globais
+const form = document.querySelector('form');
+const inputPeso = document.querySelector('#kg');
+const inputCm = document.querySelector('#cm');
+
+//funçoes
+form.onsubmit = function(event){
+    event.preventDefault() // tira a ação padrao do botão(enviar)
+
+    //pegando os valores do input
+    const peso = inputPeso.value;
+    const cm = inputCm.value;
+
+    console.log(peso,cm);
+}
