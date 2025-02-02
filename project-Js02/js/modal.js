525398a7402565e2aedc0948b292d6ef5b83084e
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2025-02-02 18:12:13 -0300

    modularizando project js2

---
+++ project-Js02/js/modal.js
@@ -0,0 +1,22 @@
+//objetos literais, peguei as variaveis globais e coloquei dentro do
+//object
+export const Modal = {
+    wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .title span'),
+    buttonClose: document.querySelector('.modal button.close'),
+
+    //duas formas diferentes de declarar o obj
+    open: function(){
+        Modal.wrapper.classList.add('open')
+    },
+    close(){
+        Modal.wrapper.classList.remove('open')
+    }
+
+}
+
+
+Modal.buttonClose.onclick = () => {
+    //modalWrapper.classList.remove('open');
+    Modal.close();
+}
--- project-Js02/js/script.js
+++ project-Js02/js/script.js
@@ -1,24 +1,10 @@
+import {Modal} from './modal.js'; //modularização
 //variaveis globais
 const form = document.querySelector('form');
 const inputPeso = document.querySelector('#kg');
 const inputCm = document.querySelector('#cm');
 
-//objetos literais, peguei as variaveis globais e coloquei dentro do
-//object
-const Modal = {
-    wrapper: document.querySelector('.modal-wrapper'),
-    message: document.querySelector('.modal .title span'),
-    buttonClose: document.querySelector('.modal button.close'),
 
-    //duas formas diferentes de declarar o obj
-    open: function(){
-        Modal.wrapper.classList.add('open')
-    },
-    close(){
-        Modal.wrapper.classList.remove('open')
-    }
-
-}
 //funçoes
 form.onsubmit = function(event){
     event.preventDefault() // tira a ação padrao do botão(enviar)
@@ -33,11 +19,6 @@ form.onsubmit = function(event){
     Modal.message.innerText = message
     //modalWrapper.classList.add('open');
     Modal.open();
-}
-
-Modal.buttonClose.onclick = () => {
-    //modalWrapper.classList.remove('open');
-    Modal.close();
 }
 function IMC(peso,cm){
     return(peso / ((cm /100) **2)).toFixed(2); // função que calcula o imc
