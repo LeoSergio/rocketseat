c7c7686f4b5c4ed8ecea60dbde4e2f79f27d7c00
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2025-02-08 08:51:58 -0300

    add button function

--- project-js03/js/script.js
+++ project-js03/js/script.js
@@ -1,17 +1,29 @@
 //DOM
-
-let play = document.querySelector('.play');
-
-let pause = document.querySelector('.pause');
+//refatoração é reorganizar semanticamente o codigo
+const buttonPlay = document.querySelector('.play');
+const buttonPause = document.querySelector('.pause');
+const buttonStop = document.querySelector('.stop');
+const buttonSet = document.querySelector('.set');
 
 //eventos direcionadas
+//programação imperativa
+//callback
 
-play.addEventListener('click', function(){
-    play.classList.add('hide');
-    pause.classList.remove('hide');
+buttonPlay.addEventListener('click', function(){
+    buttonPlay.classList.add('hide');
+    buttonPause.classList.remove('hide');
+    buttonSet.classList.add('hide');
+    buttonStop.classList.remove('hide');
 });
 
-pause.addEventListener('click', function(){
-    pause.classList.add('hide');
-    play.classList.remove('hide');
+buttonPause.addEventListener('click', function(){
+    buttonPause.classList.add('hide');
+    buttonPlay.classList.remove('hide');
+});
+
+buttonStop.addEventListener('click', function(){
+    buttonPlay.classList.remove('hide');
+    buttonPause.classList.add('hide');
+    buttonSet.classList.remove('hide');
+    buttonStop.classList.add('hide');
 })
