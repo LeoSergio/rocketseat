9081bc62321119e3cf73de12dd25668fe5295246
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2025-02-10 18:09:44 -0300

    add clear time

--- project-js03/js/script.js
+++ project-js03/js/script.js
@@ -6,9 +6,11 @@ const buttonStop = document.querySelector('.stop');
 const buttonSet = document.querySelector('.set');
 const buttonSoundOff = document.querySelector('.sound-off');
 const buttonSoundOn = document.querySelector('.sound-on');
-let minutes;
 const minutesDisplay = document.querySelector('.minutes');
 const secondsDisplay = document.querySelector('.seconds');
+let minutes = Number(minutesDisplay.textContent)
+let timerTimeOut;
+
 
 function resetControls(){
     buttonPlay.classList.remove('hide');
@@ -21,11 +23,16 @@ function updateTimerDisplay(minutes,seconds){
     minutesDisplay.textContent = String(minutes).padStart(2,"0");
     secondsDisplay.textContent = String(seconds).padStart(2,"0");
 }
+
+function resetTimer(){
+    updateTimerDisplay(minutes, 0);
+    clearTimeout(timerTimeOut);
+}
 //eventos direcionadas
 //programação imperativa
 //callback
 function countdown(){
-    setTimeout(function(){
+    timerTimeOut = setTimeout(function(){
         let seconds = Number(secondsDisplay.textContent);
         let minutes = Number(minutesDisplay.textContent);
         
@@ -62,10 +69,12 @@ buttonPlay.addEventListener('click', function(){
 buttonPause.addEventListener('click', function(){
     buttonPause.classList.add('hide');
     buttonPlay.classList.remove('hide');
+    clearTimeout(timerTimeOut);
 });
 
 buttonStop.addEventListener('click', function(){
     resetControls();
+    resetTimer();
 })
 
 buttonSoundOn.addEventListener('click', function(){
@@ -79,7 +88,12 @@ buttonSoundOff.addEventListener('click', function(){
 })
 
 buttonSet.addEventListener('click',function(){
-    minutes = prompt('Digite os minutos!');
+    let newMinutes = prompt('Digite os minutos!');
+    if (!newMinutes){
+        resetTimer();
+        return
+    }
+    minutes = newMinutes;
     updateTimerDisplay(minutes, 0);
 });
 
