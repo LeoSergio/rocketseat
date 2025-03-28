e0147fed5c473f324b2d28a31317f85b86c76050
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2025-03-28 15:45:31 -0300

    encapsulamento  POO

--- project-SPA/js/index.js
+++ project-SPA/js/index.js
@@ -1,12 +1,20 @@
-import './router.js'
+import Router from './router.js'
 //mapeamento de rotas 
-const routes ={
-    "/": "/pages/home.html",
-    "/about": "/pages/about.html",
-    "/contato": "/pages/contato.html",  
-    404: "/pages/404.html",
-    }
 
-handle()
-window.onpopstate = () => handle()
-window.route = () => route()
+const router = new Router()
+router.add('/', "/pages/home.html")
+router.add('/about', "/pages/about.html")
+router.add('/contato', "/pages/contato.html")
+router.add(404, "/pages/404.html")
+
+
+//const routes ={
+ //   "/": "/pages/home.html",
+   // "/about": "/pages/about.html",
+    //"/contato": "/pages/contato.html",  
+    //404: "/pages/404.html",
+    //}
+
+//handle()
+//window.onpopstate = () => handle()
+//window.route = () => route()
